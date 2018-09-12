37745ca5e5e7607817ce3115a91753233e64fd3f
easton <easton.pillay@outlook.com>
2018-09-11 23:28:34 -0400

    Reporting without Schoology works now, and exports the records table.
    And, we have timezone support in reporting now, so that is nice.
    Issue 2 is done!

--- modules/reports.py
+++ modules/reports.py
@@ -4,6 +4,14 @@ import datetime
 import random
 import csv
 import string
+import pytz
+import tzlocal
+import time
+import os, shutil
+
+# Get the timezone!!1!
+
+localTz = tzlocal.get_localzone()
 def exportAll():
     # Exports the records table from the db to a csv in static, then spits out the link.
     filePath = 'static/reports/exportAll-' + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(9)) + ".csv"
@@ -13,5 +21,34 @@ def exportAll():
         header = records.__table__.columns.keys()
         outcsv.writerow(header)
         for record in dbQuery:
-            outcsv.writerow([getattr(record, c) for c in header])
+            # print(record.data)
+            # outcsv.writerow([getattr(record, c) for c in header])
+            row = []
+            for c in header:
+                if c == "timeOut" or c == "timeIn":
+                    try:
+                        # convert timezone to server local time, and make it human-friendly
+                        dbTime = pytz.utc.localize(getattr(record, c)).astimezone(localTz)
+                        dbTime = dbTime.date.strftime("%D %r")
+                        row.append(dbTime)
+                    except:
+                        # If it's blank for some reason, we need to keep going.
+                        row.append(getattr(record, c))
+                else:
+                    row.append(getattr(record, c))
+            outcsv.writerow(row)
     return filePath
+
+def clearFolder():
+    # Thanks, https://stackoverflow.com/a/185941 !
+    folder = "static/reports/"
+    for file in os.listdir(folder):
+        filePath = os.path.join(folder, file)
+        try:
+            if os.path.isfile(filePath):
+                os.unlink(filePath)
+        except:
+            print("Uhh, we couldn't clear the reports folder. Something may be up with the filesystem...")
+
+# Runs on import to make sure we keep the folder tidy, since everything is just queries against the db anyway.
+clearFolder()
