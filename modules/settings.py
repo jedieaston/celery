af1b6b5faa032b72e5b755d7f1e5f260c499e9ee
easton <easton.pillay@outlook.com>
2018-09-21 20:48:39 -0400

    Database and Schoology fixes, more comments, refactoring, etc.

--- modules/settings.py
+++ modules/settings.py
@@ -1,27 +1,22 @@
 import yaml
-defaultSettings = {'db': {'url': 'postgres+psycopg2://postgres:celery@db:5432/celerydb'},
- 'general': {'signIn': True},
- 'ldap': {'ldapAccessDomain': 'example',
-  'ldapAccessPassword': 'Passw0rd!',
-  'ldapAccessUserName': 'Unprivileged',
-  'ldapAuthenticationStandard': 'NTLM',
-  'ldapAvailable': False,
-  'ldapSearchBase': 'DC=example, DC=com',
-  'ldapServer': 'ad.celery.internal'},
- 'schoology': {'apiKey': 'buymeatendiesub',
-  'apiSecret': 'skdfjlasfaslaksdlaslasfasflkasdkf3e4rijowejrf',
-  'instanceUrl': 'hogwarts.schoology.com',
-  'requestLimit': '5000'}}
+
+# The default settings, which should get it running presuming its using the usual
+# docker setup.
+
+defaultSettings = {'ldap': {'ldapAvailable': True, 'ldapServer': 'example.com', 'ldapAccessDomain': 'example',
+                            'ldapAccessUserName': 'Unprivileged', 'ldapAccessPassword': 'Passw0rd!',
+                            'ldapSearchBase': 'DC=example, DC=com', 'ldapAuthenticationStandard': 'NTLM'},
+                   'general': {'signIn': True}, 'db': {'url': 'postgres+psycopg2://postgres:celery@db:5432/celerydb'},
+                   'schoology': {'instanceUrl': 'xaviersschool.schoology.com', 'apiKey': 'buymeatendiesub',
+                                 'apiSecret': 'skdfjlasfaslaksdlaslasfasflkasdkf3e4rijowejrf', 'requestLimit': '5000'},
+                   'dev': {'debug': True}}
+
 
 def updateSettingsDicts(**kwargs):
     # updates settings before writing them to file.
-    #print(kwargs.items())
     for key, value in kwargs.items():
         value.pop('csrf_token')
         settings[key] = value
-    #currentSettingsYaml = yaml.safe_dump(settings, default_flow_style=False)
-    #print(currentSettingsYaml)
-    #writeSettings()
     with open("config/settings.yaml", "w") as settingsYaml:
         yaml.safe_dump(settings, settingsYaml, default_flow_style=False)
 
@@ -30,25 +25,6 @@ def writeSettings():
     with open("config/settings.yaml", "w") as settingsYaml:
         yaml.safe_dump(settings, settingsYaml, default_flow_style=False)
 
-# while True:
-#     try:
-#         with open("config/settings.yaml", "r+") as settingsFile:
-#             settings = yaml.safe_load(settingsFile)
-#             if settings == None:
-#                 raise Exception("Woah, there's nothing in the settings file. Restoring defaults.")
-#         break
-#     except:
-#         try:
-#             # If settings don't exist, create new settings file.
-#             with open('config/settings.yaml', 'a+') as newSettings:
-#                 yaml.safe_dump(defaultSettings, newSettings, default_flow_style=False)
-#             with open("config/settings.yaml") as settings:
-#                 settings = yaml.safe_load(settings)
-#             break
-#         except:
-#             print("We're having issues interacting with your file system, meaning we can't create a settings file. Exiting.")
-#             exit()
-#         continue
 
 while True:
     try:
@@ -68,9 +44,9 @@ while True:
             print("We're having issues interacting with your filesystem, and cannot "
                   "save a configuration file. Exiting.")
             exit()
-# with open("config/settings.yaml", "r") as settings:
-#     settings = yaml.load(settings)
+
 ldap = settings["ldap"]
 general = settings["general"]
 db = settings["db"]
 schoology = settings["schoology"]
+dev = settings["dev"]
